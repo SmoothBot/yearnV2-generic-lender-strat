86d9c48cfb293700b10d00ff0a752bef9bb44356
iHeroN <iherondev@gmail.com>
2022-04-19 08:52:24 +0200

    Made a GenericWithParameters strat
    This strat only depends on the GenericLenderParameters

---
+++ contracts/Interfaces/GenericLender/GenericLenderParameters.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.6.12;
+
+struct GenericLenderParameters { 
+    //These parameters changes for every different chain
+    uint256 _blocksPerYear;
+    address _uniswapRouter;
+    address _weth;
+    //These parameters changes for every different protocol
+    address _comp;
+    address _name;
+    //These parameters changes for every different token/strategy
+    address _cToken;
+    address _strategy;
+
+}
